70bcb203efc2121d41e37d30fa601c206accdfb0
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-05-20 16:34:56 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,19 @@
 def solution(s):
-    arr = list(map(int, s.split(' ')))         
-    arr.sort()                                  
-    return str(arr[0]) + " " + str(arr[-1])
+    arr = []
+    num_str = ""
+    for i in range(len(s)):
+        if s[i] == " ":
+            arr.append(num_str)
+            num_str = ""
+        else:
+            num_str += s[i]
+    arr.append(num_str)
+    
+    min_num, max_num = int(arr[0]), int(arr[0])
+    for num in arr[0:]:
+        if int(num) < min_num:
+            min_num = int(num)
+        if int(num) > max_num:
+            max_num = int(num)
+
+    return str(min_num) + " " + str(max_num)
