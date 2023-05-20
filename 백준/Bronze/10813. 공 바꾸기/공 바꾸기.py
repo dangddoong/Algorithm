36370ecd5d0658cb5af9d39f1d213493e0975881
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-05-21 01:22:06 +0900

    [Bronze II] Title: 공 바꾸기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,12 +1,8 @@
-import sys
-readl = sys.stdin.readline
+n, m = map(int, input().split())
+answer = [i for i in range(1, n+1)]
 
-n,m = [int(i) for i in readl().split()]
-rows = [[int(i) for i in readl().split()] for _ in range(m)]
-li = [i for i in range(1, n+1)]
+for _ in range(m):
+    i, j = map(int, input().split())
+    answer[i-1], answer[j-1] = answer[j-1], answer[i-1]
 
-for i in range(m):
-    mock = li[rows[i][0]-1]
-    li[rows[i][0]-1] = li[rows[i][1]-1]
-    li[rows[i][1]-1] = mock
-print(' '.join(str(n) for n in li))
+print(' '.join(str(n) for n in answer))
