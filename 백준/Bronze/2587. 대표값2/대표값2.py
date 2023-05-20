a31ec2a4362588e3950132b3bb044de7608a6b13
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-05-20 16:17:20 +0900

    Update 대표값2.py

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,6 +1,19 @@
-x = []
-for i in range(5):
-    x.append(int(input()))
-x.sort()
-print(int(sum(x)/5))
-print(x[2])
+li = []
+for _ in range(5):
+    li.append(int(input()))
+for i in range(len(li)):
+    for j in range(i+1,len(li)):
+        if li[i]>li[j]: 
+            temp = li[i]
+            li[i] = li[j]
+            li[j] = temp
+print(int(sum(li)/5))
+print(li[2])
+
+# sort()로 풀면 쉽다.
+# x = []
+# for i in range(5):
+#     x.append(int(input()))
+# x.sort()
+# print(int(sum(x)/5))
+# print(x[2])
