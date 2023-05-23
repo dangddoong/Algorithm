e16acf185f48a0aee68856e2b8f28a907f0a73bc
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-05-23 15:49:07 +0900

    Update 2진수 뒤집기.py

--- 백준/Bronze/11179. 2진수 뒤집기/2진수 뒤집기.py
+++ 백준/Bronze/11179. 2진수 뒤집기/2진수 뒤집기.py
@@ -1,3 +1,18 @@
+# 맛있게 풀기
+import sys
+readl = sys.stdin.readline
+n = int(readl())
+answer = 0
+
+while n!=0:
+    answer = answer*2 + (n%2)
+    n = n//2
+    
+print(answer)
+
+
+
+#  deque로 풀기
 import sys
 from collections import deque
 readl = sys.stdin.readline
