365fad262b512f53a3e32df0e3a10cead247a983
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-05-23 23:03:08 +0900

    [Silver IV] Title: 스택, Time: 180 ms, Memory: 115576 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,28 @@
+import sys
+readl = sys.stdin.readline
+n = int(readl())
+stack = []
+answer = ""
+
+for _ in range(n):
+    command = readl().split()
+    if command[0] == "push":
+        stack.append(command[1])
+    elif command[0] == "pop":
+        if len(stack)==0:
+            answer += "-1" + "\n"
+        else:
+            answer += stack.pop() + "\n"
+    elif command[0] == "size":
+        answer += str(len(stack)) + "\n"
+    elif command[0] == "empty":
+        if len(stack)==0:
+            answer += "1" + "\n"
+        else:
+            answer += "0" + "\n"
+    elif command[0] == "top":
+        if len(stack)==0:
+            answer += "-1" + "\n"
+        else:
+            answer += stack[-1] + "\n"
+print(answer)
