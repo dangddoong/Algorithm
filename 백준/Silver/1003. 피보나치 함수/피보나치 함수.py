af1ce1c54586ccbddf771e4b857044ebe1daf3ed
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-06-08 16:02:53 +0900

    list 기반 bottom up 방식 풀이 추가

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -17,3 +17,23 @@ def fibo(n):
 for _ in range(t):
     answer = fibo(int(readl()))
     print(*answer)
+
+# 위는 dict&재귀 기반 TopDown 방식
+# 아래는 list기반 BottomUp 방식
+
+# import sys
+
+# readl = sys.stdin.readline
+# t = int(readl())
+# zero_fibo, one_fibo = [1, 0], [0, 1]
+
+# for _ in range(t):
+#     n = int(readl())
+#     len_zero = len(zero_fibo)
+#     if len_zero <= n:
+#         for i in range(len_zero, n + 1):
+#             zero_fibo.append(zero_fibo[i-1] + zero_fibo[i-2])
+#             one_fibo.append(one_fibo[i-1] + one_fibo[i-2])
+
+#     answer = [zero_fibo[n], one_fibo[n]]
+#     print(*answer)
