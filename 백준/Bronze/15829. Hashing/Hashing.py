6bea0ba417d15daef821c9ad50a7d487f7b1e28c
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-08-08 18:58:56 +0900

    Update Hashing.py

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -6,9 +6,26 @@ r_list = [1]
 for i in range(1, int(readl())):
     r_list.append(r_list[i-1] * 31)
 s = readl().rstrip()
+ord_a = ord('a')
 answer = 0
 
 for i in range(len(s)):
-    answer += (ord(s[i]) - ord('a') + 1) * r_list[i]
+    answer += (ord(s[i]) - ord_a + 1) * r_list[i]
 
-print(answer)
+print(answer % 1234567891)
+
+
+# import sys
+
+# readl = sys.stdin.readline
+
+# r_list = [1]
+# for i in range(1, int(readl())):
+#     r_list.append(r_list[i-1] * 31)
+# s = readl().rstrip()
+# answer = 0
+
+# for i in range(len(s)):
+#     answer += (ord(s[i]) - ord('a') + 1) * r_list[i]
+
+# print(answer)
