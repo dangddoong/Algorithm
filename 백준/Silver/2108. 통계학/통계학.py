1247e8bb8f70bb0e6162191de6077c85ae95ade5
조성현 <116885047+dangddoong@users.noreply.github.com>
2023-08-21 23:11:13 +0900

    [Silver III] Title: 통계학, Time: 384 ms, Memory: 148984 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,27 @@
+import sys
+readl = sys.stdin.readline
+n = int(readl())
+num = []
+for _ in range(n):
+    num.append(int(readl()))
+num.sort()
+
+print(round(sum(num)/n)) #산술평균
+print(num[(n-1)//2]) #중앙값
+
+#최빈값
+cnt = {}
+for i in range(n):
+    if num[i] not in cnt:
+        cnt[num[i]] = 1
+    else:
+        cnt[num[i]] += 1
+cnt = sorted(cnt.items(), key = lambda x:(-x[1], x[0]))
+for i in range(len(cnt)):
+    cnt[i] = list(cnt[i])
+if len(cnt) == 1 or cnt[0][1] != cnt[1][1]:
+    print(cnt[0][0])
+else:
+    print(cnt[1][0])
+
+print(max(num) - min(num)) #범위
